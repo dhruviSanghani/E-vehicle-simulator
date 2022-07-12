380447e5fae67ab7a5f25bbc7535d49b08dd8f51
Dhruvi Sanghani <dhruvishah1191@gmail.com>
2022-07-12 17:41:30 -0400

    fixed untracked files

--- E-vehicle-simulator/E-vehicle-simulator/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace E_vehicle_simulator.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
